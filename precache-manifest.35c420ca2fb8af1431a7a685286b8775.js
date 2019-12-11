6a2c83afced0f777debb948cd07298ea13ba6d57
oscar flores <oscar.flores@globant.com>
2019-12-10 18:11:43 -0600

    Updates

---
+++ precache-manifest.35c420ca2fb8af1431a7a685286b8775.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f521c970438b50e409c94c70d31d85",
+    "url": "/curso-reactjs-noticias/index.html"
+  },
+  {
+    "revision": "22f3c3397349966734d3",
+    "url": "/curso-reactjs-noticias/static/css/main.26029b58.chunk.css"
+  },
+  {
+    "revision": "05e33f589d2c2fb6f0a2",
+    "url": "/curso-reactjs-noticias/static/js/2.e0aab205.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/curso-reactjs-noticias/static/js/2.e0aab205.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f3c3397349966734d3",
+    "url": "/curso-reactjs-noticias/static/js/main.d97237a0.chunk.js"
+  },
+  {
+    "revision": "4a86a1540a6fea4d2130",
+    "url": "/curso-reactjs-noticias/static/js/runtime-main.451d9819.js"
+  }
+]);
